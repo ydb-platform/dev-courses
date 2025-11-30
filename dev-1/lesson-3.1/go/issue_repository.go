480c2618496a8b4ccf48a8f1825f4ee3dc4caeac
danilov6083 <danilov6083@outlook.com>
2025-12-01 01:05:56 +0300

    added comments

--- dev-1/lesson-3.1/go/issue_repository.go
+++ dev-1/lesson-3.1/go/issue_repository.go
@@ -17,6 +17,8 @@ var (
 	)
 )
 
+// Репозиторий для работы с тикетами в базе данных YDB
+// Реализует операции добавления и чтения тикетов
 type IssueRepository struct {
 	query *QueryHelper
 }
@@ -27,10 +29,17 @@ func NewIssueRepository(query *QueryHelper) *IssueRepository {
 	}
 }
 
+// Добавление нового тикета в БД
+// ctx   [context.Context] - контекст для управления исполнением запроса (например, можно задать таймаут)
+// title [string] - название тикета
+// Возвращает созданный тикет или ошибку
 func (repo *IssueRepository) AddIssue(ctx context.Context, title string) (*Issue, error) {
+	// Генерируем случайный id для тикета
 	id := random.Int63() // do not repeat in production
 	timestamp := time.Now()
 
+	// Выполняем UPSERT запрос для добавления тикета
+	// Изменять данные можно только в режиме транзакции SERIALIZABLE_RW, поэтому используем его
 	err := repo.query.ExecuteTx(`
 		DECLARE $id AS Int64;
 		DECLARE $title AS Text;
@@ -58,9 +67,17 @@ func (repo *IssueRepository) AddIssue(ctx context.Context, title string) (*Issue
 	}, nil
 }
 
+// Возвращает тикет по заданному id
+// ctx [context.Context] - контекст для управления исполнением запроса (например, можно задать таймаут)
+// id  [int64] - id тикета
+// Возвращает найденный тикет или ошибку
 func (repo *IssueRepository) FindById(ctx context.Context, id int64) (*Issue, error) {
 	result := make([]Issue, 0)
 
+	// Выполняем SELECT запрос в режиме [Snapshot Read-Only] для чтения данных
+    // Этот режим сообщает серверу, что эта транзакция только для чтения.
+    // Это позволяет снизить накладные расходы на подготовку к изменениям 
+	// и просто читать данные из одного "слепка" базы данных.
 	repo.query.Query(`
 		SELECT
 			id,
@@ -76,6 +93,10 @@ func (repo *IssueRepository) FindById(ctx context.Context, id int64) (*Issue, er
 			Build(),
 		func(resultSet query.ResultSet, ctx context.Context) error {
 			for row, err := range sugar.UnmarshalRows[Issue](resultSet.Rows(ctx)) {
+				// Если во время чтения результата возникла ошибка,
+				// то необходимо очистить уже прочитанные результаты, 
+				// чтобы избежать дублирования при следующем выполнении функции-ретраера
+				// и вернуть ретраеру ошибку
 				if err != nil {
 					clear(result)
 					return err
@@ -88,17 +109,23 @@ func (repo *IssueRepository) FindById(ctx context.Context, id int64) (*Issue, er
 	)
 
 	if len(result) > 1 {
-		return nil, errors.New("Multiple rows with the same id")
+		return nil, errors.New("Multiple rows with the same id (lol)")
 	}
 	if len(result) == 0 {
-		return nil, errors.New("Did not find any issues (lol)")
+		return nil, errors.New("Did not find any issues")
 	}
 	return &result[0], nil
 }
 
+// Получает все тикеты из базы данных
+// ctx [context.Context] - контекст для управления исполнением запроса (например, можно задать таймаут)
 func (repo *IssueRepository) FindAll(ctx context.Context) ([]Issue, error) {
 	result := make([]Issue, 0)
 
+	// Выполняем SELECT запрос в режиме [Snapshot Read-Only] для чтения данных
+    // Этот режим сообщает серверу, что эта транзакция только для чтения.
+    // Это позволяет снизить накладные расходы на подготовку к изменениям 
+	// и просто читать данные из одного "слепка" базы данных.
 	err := repo.query.Query(`
 		SELECT
 			id,
@@ -111,6 +138,10 @@ func (repo *IssueRepository) FindAll(ctx context.Context) ([]Issue, error) {
 		ydb.ParamsBuilder().Build(),
 		func(resultSet query.ResultSet, ctx context.Context) error {
 			for row, err := range sugar.UnmarshalRows[Issue](resultSet.Rows(ctx)) {
+				// Если во время чтения результата возникла ошибка,
+				// то необходимо очистить уже прочитанные результаты, 
+				// чтобы избежать дублирования при следующем выполнении функции-ретраера
+				// и вернуть ретраеру ошибку
 				if err != nil {
 					clear(result)
 					return err
--- dev-1/lesson-3.1/go/schema_repository.go
+++ dev-1/lesson-3.1/go/schema_repository.go
@@ -5,6 +5,8 @@ import (
 	"log"
 )
 
+// Репозиторий для управления схемой базы данных YDB
+// Отвечает за создание и удаление таблиц
 type SchemaRepository struct {
 	query *QueryHelper
 }
@@ -15,6 +17,12 @@ func NewSchemaRepository(query *QueryHelper) *SchemaRepository {
 	}
 }
 
+// Создает таблицу issues в базе данных
+// Таблица содержит поля:
+// - id: уникальный идентификатор тикета
+// - title: название тикета
+// - created_at: время создания тикета
+// Все поля являются обязательными.
 func (repo *SchemaRepository) CreateSchema(ctx context.Context) {
 	err := repo.query.Execute(`
 		CREATE TABLE IF NOT EXISTS issues (
@@ -31,6 +39,8 @@ func (repo *SchemaRepository) CreateSchema(ctx context.Context) {
 	}
 }
 
+// Удаляет таблицу issues из базы данных
+// Используется для очистки схемы перед созданием новой
 func (repo *SchemaRepository) DropSchema(ctx context.Context) {
 	err := repo.query.Execute("DROP TABLE IF EXISTS issues;", ctx)
 	if err != nil {
